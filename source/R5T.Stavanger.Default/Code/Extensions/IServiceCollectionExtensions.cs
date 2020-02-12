5672385a65b442096d900716e423eb5a2f3aea85
MinexAutomation <minex.automation@gmail.com>
2020-02-11 21:24:17 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Stavanger.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Stavanger.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="ShortcutPathConventions"/> implementation of <see cref="IShortcutPathConventions"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultShortcutPathConventions(this IServiceCollection services)
+        {
+            services.AddSingleton<IShortcutPathConventions, ShortcutPathConventions>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ShortcutPathConventions"/> implementation of <see cref="IShortcutPathConventions"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IShortcutPathConventions> AddDefaultShortcutPathConventionsAction(IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IShortcutPathConventions>(() => services.AddDefaultShortcutPathConventions());
+            return serviceAction;
+        }
+    }
+}
